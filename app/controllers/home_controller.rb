8957dfc10a7e71528bab6a58603fcb4cccb4fff4
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-11-21 11:18:11 +1100

    Add candidate listing page and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  # before_action :authenticate_user!
+
   def index
-    
+
   end
+
+  def candidates
+    @candidates = User.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :searches
   devise_for :users
   root 'home#index'
+  get 'home/candidates', to: 'home#candidates', as: 'candidates'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
