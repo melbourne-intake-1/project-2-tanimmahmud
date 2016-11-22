3522b9f4cff6178bf945a68010d7a4180105e19f
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-11-22 19:16:10 +1100

    add select feild for job form

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -9,4 +9,30 @@ module JobsHelper
       ['Internship']
     ]
   end
+
+  def positions
+    [
+      ['Full Stack'],
+      ['Front End'],
+      ['Back End'],
+      ['UX/UI'],
+      ['Web Designer']
+    ]
+  end
+
+  def locations
+    [
+      ['South Australia'],
+      ['Tasmania'],
+      ['New South Wales'],
+      ['Victoria'],
+      ['Western Australia'],
+      ['Queensland'],
+      ['Australian Capital Territory'],
+      ['Northern Territory'],
+      ['Christmas Island'],
+      ['Cocos (Keeling) Islands'],
+      ['Norfolk Island']
+    ]
+  end
 end
