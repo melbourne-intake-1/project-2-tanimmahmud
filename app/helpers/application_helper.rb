ae4dcdff0dda485cac4685db1866a989d63f7b52
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-11-28 14:28:48 +1100

    resize profile picture and show profile pic in inbox

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     ]
   end
 
+  def active_page(active_page)
+    @active == active_page ? "active" : ""
+  end
+
   def user_avatar(user)
     if user.profile.image.present?
       image_tag user.profile.image_url(:thumb)
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   has_many :skills
   acts_as_messageable
 
+  def mailboxer_name
+    self.name
+  end
+
   def mailboxer_email(object)
     self.email
   end
