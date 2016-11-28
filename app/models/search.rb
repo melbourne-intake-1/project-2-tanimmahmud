754bde71de4c1c7e360eba62ea0a3b40326a4fa0
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-11-29 01:10:06 +1100

    add candidate search method

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,11 +2,22 @@ class Search < ApplicationRecord
 
   def search_jobs
 
-    jobs = Job.all
+    jobs = Job.where('status': 'approved')
 
     jobs = jobs.where(["job_type LIKE?", "%#{job_type}%"]) if job_type.present?
     jobs = jobs.where(["position LIKE?", "%#{position}%"]) if position.present?
     jobs = jobs.where(["location LIKE?", "%#{location}%"]) if location.present?
+
+    return jobs
+  end
+
+  def candidates
+
+    candidates = User.joins(:profile).where('role': 'candidate')
+
+    candidates = candidates.profile.where(["name LIKE?", "%#{name}%"]) if job_type.present?
+    candidates = candidates.profile.where(["position LIKE?", "%#{position}%"]) if position.present?
+    candidates = candidates.profile.where(["location LIKE?", "%#{location}%"]) if location.present?
 
     return jobs
   end
