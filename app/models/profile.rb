1f1136d3d59d2fa475b99fa7b8fa5db7f17b64ed
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-11-28 11:47:48 +1100

    Add image and resume file upload size validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,7 +6,16 @@ class Profile < ApplicationRecord
   validates :image, presence: true
   validates :address, presence: true
   validates :description, length: { minimum: 250 }
+  validate :size_validation, :if => "image?" || "resume_file?"
+  
+
+
 
   mount_uploader :image, AvatarUploader
   mount_uploader :resume_file, ResumeUploader
+
+  def size_validation
+    errors[:image] << "should be less than 1MB" if image.size > 1.megabytes
+    errors[:resume_file] << "should be less than 1MB" if resume_file.size > 1.megabytes
+  end
 end
