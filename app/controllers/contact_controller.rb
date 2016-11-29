e3fe65bb8c28da17885ad204beb62fd29a189ede
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-11-29 18:32:54 +1100

    edit contact controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -12,7 +12,7 @@ class ContactController < ApplicationController
 
     if (email.present? && message.present?)
       # Tell the ContactMailer to send an email
-      ContactMailer.send_contact_email(name,email,subject,message).deliver_later
+      ContactMailer.send_contact_email(name,email,subject,message).deliver_now
       flash[:success] = 'Email sent! Thank you for you email.'
       redirect_to root_path, notice: 'Email sent!'
     else
